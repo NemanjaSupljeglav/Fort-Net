2ecad84ee8ff15e1fd56c200ff14a22a3172950f
Nemanja Šupljeglav <nsupljeglav@ad.ito.ba>
2021-11-22 11:09:06 +0100

    first commit

--- fort-net-app/src/components/postsCard/PostCard.js
+++ fort-net-app/src/components/postsCard/PostCard.js
@@ -3,12 +3,12 @@ import "./postCard.scss";
 function PostCard() {
   return (
     <div className="post-card-wrapper">
-      <p className="post-type">Active Tourism</p>
+      <p className="post-type">Active tourism</p>
       <img
         className="post-cover-image"
         src="https://fort-net.org/wp-content/uploads/2021/07/20210624125807_Pjesacenje-1-1024x683.jpg"
       ></img>
-      <h2 className="post-card-title">Pjesacenje</h2>
+      <h2 className="post-card-title">pjesacenje</h2>
     </div>
   );
 }

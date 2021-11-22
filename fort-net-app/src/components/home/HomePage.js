eee212366703122fe818f6aac4b642fd4b37a677
Nemanja Šupljeglav <nsupljeglav@ad.ito.ba>
2021-11-22 15:06:16 +0100

    Map data

--- fort-net-app/src/components/home/HomePage.js
+++ fort-net-app/src/components/home/HomePage.js
@@ -1,8 +1,36 @@
 import PostCard from "../postsCard/PostCard";
+import "./homePage.scss";
+import { useState } from "react";
+import { useEffect } from "react";
 function HomePage() {
+  const [post, setPost] = useState([]);
+  const [postLoad, setPostLoad] = useState(false);
+
+  const getPosts = async () => {
+    const res = await fetch(
+      "https://staging-api.fort-net.org/Objects?Skip=1&Take=50"
+    );
+    const posts = await res.json();
+    console.log(posts.result);
+    setPost(posts.result);
+    setPostLoad(true);
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div className="home-page-wrapper">
-      <PostCard></PostCard>
+      {post.map((post) => {
+        return (
+          <PostCard
+            key={post.id}
+            postImages={post.photoUrl}
+            objectType={post.objectType}
+            name={post.name}
+          ></PostCard>
+        );
+      })}
     </div>
   );
 }
--- fort-net-app/src/components/postsCard/PostCard.js
+++ fort-net-app/src/components/postsCard/PostCard.js
@@ -1,14 +1,36 @@
 import "./postCard.scss";
 
-function PostCard() {
+function PostCard(props) {
+  const ObjectType = [
+    "Object",
+    "Accommodation",
+    "EnoGastro",
+    "Heritage",
+    "Tourism",
+  ];
+
   return (
     <div className="post-card-wrapper">
-      <p className="post-type">Active tourism</p>
-      <img
-        className="post-cover-image"
-        src="https://fort-net.org/wp-content/uploads/2021/07/20210624125807_Pjesacenje-1-1024x683.jpg"
-      ></img>
-      <h2 className="post-card-title">pjesacenje</h2>
+      <p className="post-type">{ObjectType[props.objectType]}</p>
+      {props.postImages === null && (
+        <img
+          className="post-cover-image"
+          src="noImages.png"
+          alt="Post Image"
+        ></img>
+      )}
+      {props.postImages && (
+        <img
+          className="post-cover-image"
+          src={props.postImages}
+          alt="Post Image"
+        ></img>
+      )}
+
+      <h2 className="post-card-title">{props.name}</h2>
+      {props.name == null && (
+        <h2 className="post-card-title">There is no active name</h2>
+      )}
     </div>
   );
 }
